8713989a6963258b244b93ea74c8af009676dff1
j_file <j_file@gmail.com>
2023-02-06 05:14:27 +0530

    b.py added lamda function for stop an instance (weekly shortdown

--- b.py
+++ b.py
@@ -1,4 +1,13 @@
-print("b py file cteated")
-'''
-2nd commit
-'''
+import boto3
+
+def lambda_handler(event, context):
+    # Create an EC2 client
+    ec2 = boto3.client('ec2')
+
+    # Identify the instance you want to stop by using its instance ID
+    instance_id = 'your_instance_id'
+
+    # Stop the instance
+    ec2.stop_instances(InstanceIds=[instance_id])
+
+    return "Instance stopped successfully"
